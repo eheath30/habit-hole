e7dad63d48ceaf10d66231545fb45f22bebe9599
Rameez <rameez0khawaja@gmail.com>
2022-07-18 15:22:45 +0100

    seed password hashed

--- db/2_seeds.sql
+++ db/2_seeds.sql
@@ -1,3 +1,15 @@
 INSERT INTO users (username, password_digest)
-VALUES('username', 'password123')
+VALUES('username', '$2a$10$9LR1x94XKwxDVoWRw6CZA.VWXMPD7I9wpI.zKcJMTCdgNwduIFSEO') --Password is password
 
+-- INSERT INTO habits
+
+
+
+
+-- CREATE TABLE habits (
+--     id serial PRIMARY KEY,
+--     user_id int,
+--     sleep_target int,
+--     sleep_date date[10],
+--     sleep_hours int[10]
+-- );
