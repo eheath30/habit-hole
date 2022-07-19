71220fb17b7e9de6ed792e08a4ec153e9158eab1
eheath30 <elliot.heath@btinternet.com>
2022-07-19 10:34:27 +0100

    adding dom testing

---
+++ client/tests/index.test.js
@@ -0,0 +1,35 @@
+// const appFile = require('../js/app');
+// // const fs = require('fs');
+// // const path = require('path');
+// // const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
+
+// //for fetch calls
+// global.fetch = require('jest-fetch-mock');
+
+// // const functions = require('../js/app.js');
+
+// // jest.mock('../js/app.js');
+
+// beforeEach(() => { fetch.resetMocks() })
+
+// //TESTING
+
+// describe('testing forEach in appendResults', () => {
+//     test('accepts json', () => {
+//         functions.appendResults = jest.fn()
+//         // const testArray = ['this', 'that', 'the other'];
+//         // functions.appendResults(testArray);
+//         expect(functions.appendResults([1,2,3,4,5])).toHaveBeenCalledTimes(5);
+//         })
+//     })
+
+
+// global.fetch = require('jest-fetch-mock');
+// // let app;
+
+// describe('testing fetch requst', () => {
+//     test("testing fetch data", () => {
+//         appFile.fetchData;
+//         expect(fetch).toHaveBeenCalledWith('http://localhost:5000/posts/');
+//     })
+// })
