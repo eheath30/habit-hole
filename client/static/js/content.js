a9d80a90c58b166714fa0974d20a74d96cd05ca0
eheath30 <elliot.heath@btinternet.com>
2022-07-20 20:06:52 +0100

    updated main

--- client/static/js/content.js
+++ client/static/js/content.js
@@ -1,41 +1,49 @@
 function renderHomepage(){
     const homepage = document.createElement('div');
     homepage.innerHTML = `<div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
-    <div class="col-md-6 px-0">
-      <h1 class="display-4 fst-italic">Title of a longer featured blog post</h1>
-      <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
-      <p class="lead mb-0"><a href="#" class="text-white fw-bold">Continue reading...</a></p>
+    <div class="d-flex flex-row">
+    <div class="col-md-6 px-0 py-2">
+      <h1 class="display-4 fst-italic">Sleep better, today.</h1>
+      <p class="lead my-3">From world renowned developers comes habit-hole,
+      <br>
+      a revolutionary sleep tracking web-app.</p>
+    </div>
+    <div class="col-md-6 px-0 ">
+      <img class="img-responsive align-center w-100 h-100 d-none d-lg-block" src="./static/images/forest.jpg" alt="forest" />
+    </div>
     </div>
   </div>
-
   <div class="row mb-2">
     <div class="col-md-6">
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-primary">World</strong>
-          <h3 class="mb-0">Featured post</h3>
-          <div class="mb-1 text-muted">Nov 12</div>
-          <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
-        </div>
-        <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
+          <strong class="d-inline-block mb-2 text-success">Health & Fitness</strong>
+          <h3 class="mb-0">Benefits of tracking your sleep</h3>
+          <div class="mb-2 text-muted mt-2">July 18</div>
+          <p class="card-text mb-auto mt-2">Decreased risk of serious medical conditions.</p>
+          <p class="card-text mb-auto mt-2">Improved mood.</p>
+          <p class="card-text mb-auto mt-2">Better cognitive functioning.</p>
+          <p class="card-text mb-auto mt-2">Improved immunity.</p>
+          <p class="card-text mb-auto mt-2">Better weight control.</p>
+          <p class="card-text mb-auto mt-2">Better sleep efficiency.</p>
         </div>
       </div>
     </div>
     <div class="col-md-6">
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-success">Design</strong>
-          <h3 class="mb-0">Post title</h3>
-          <div class="mb-1 text-muted">Nov 11</div>
-          <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
+          <strong class="d-inline-block mb-2 text-primary">Company News</strong>
+          <h3 class="mb-0">habit-hole</h3>
+          <div class="mb-1 text-muted mt-2">July 18</div>
+          <p class="mb-auto mt-1">Habit hole is an app developed to help users track their sleep patterns, compare their own historial sleep data and improve their quality of life.</p>
+          <p class="mb-auto mt-2">This application was designed and developed by 3 full-stack engineers working as part of the futureproof training programme.</p>
+          <div class="d-flex bd-highlight my-2">
+          <div class="p-2 flex-fill bd-highlight">
+          <a href="https://github.com/uzair14" class="text-decoration-none" target="_blank">Uzair <i class="fa-brands fa-github"></i></a>
+          </div>
+          <div class="p-2 flex-fill bd-highlight"><a href="https://github.com/rameez-khawaja" class="text-decoration-none" target="_blank">Rameez <i class="fa-brands fa-github"></i></a></div>
+          <div class="p-2 flex-fill bd-highlight"><a href="https://github.com/eheath30" class="text-decoration-none" target="_blank">Elliot <i class="fa-brands fa-github"></i></a></div>
         </div>
-        <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
         </div>
       </div>
     </div>
@@ -45,9 +53,9 @@ main.appendChild(homepage);
 
 function renderLoginForm() {
     const fields = [
-        { tag: 'input', attributes: { type: 'text', name: 'username', placeholder: 'Username' } },
-        { tag: 'input', attributes: { type: 'password', name: 'password', placeholder: 'Password' } },
-        { tag: 'input', attributes: { type: 'submit', value: 'Login' } }
+        { tag: 'input', attributes: { type: 'text', name: 'username', placeholder: 'Username', class:"form-control my-1"} },
+        { tag: 'input', attributes: { type: 'password', name: 'password', placeholder: 'Password', class:"form-control my-1"} },
+        { tag: 'input', attributes: { type: 'submit', value: 'Login', class:"btn btn-dark my-1"} }
     ]
     const form = document.createElement('form');
     fields.forEach(f => {
@@ -58,15 +66,23 @@ function renderLoginForm() {
         })
     })
     form.addEventListener('submit', requestLogin)
-    main.appendChild(form);
+
+    let title = document.createElement('h3');
+    title.textContent = 'Login';
+    let card = document.createElement('div');
+    card.appendChild(title);
+     card.setAttribute('class', 'p-4 p-md-5 mb-4 text-white rounded bg-secondary');
+     card.appendChild(form)
+    main.appendChild(card);
+
 }
 
 function renderRegisterForm() {
     const fields = [
-        { tag: 'input', attributes: { type: 'text', name: 'username', placeholder: 'Username' } },
-        { tag: 'input', attributes: { type: 'password', name: 'password', placeholder: 'Password' } },
-        { tag: 'input', attributes: { type: 'password', name: 'passwordConfirmation', placeholder: 'Confirm Password' } },
-        { tag: 'input', attributes: { type: 'submit', value: 'Create Account' } }
+        { tag: 'input', attributes: { type: 'text', name: 'username', placeholder: 'Username', class:"form-control my-1" } },
+        { tag: 'input', attributes: { type: 'password', name: 'password', placeholder: 'Password', class:"form-control my-1" } },
+        { tag: 'input', attributes: { type: 'password', name: 'passwordConfirmation', placeholder: 'Confirm Password', class:"form-control my-1" } },
+        { tag: 'input', attributes: { type: 'submit', value: 'Create Account', class:"btn btn-dark my-1"} }
     ]
     const form = document.createElement('form');
     fields.forEach(f => {
@@ -77,7 +93,13 @@ function renderRegisterForm() {
         })
     })
     form.addEventListener('submit', requestRegistration)
-    main.appendChild(form);
+    let title = document.createElement('h3');
+    title.textContent = 'Register for a free account';
+    let card = document.createElement('div');
+    card.appendChild(title);
+     card.setAttribute('class', 'p-4 p-md-5 mb-4 text-white rounded bg-secondary');
+     card.appendChild(form)
+    main.appendChild(card);
 }
 
 async function renderFeed() {
